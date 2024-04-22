f36d75166abcb7c99985efecdfc441ded69cb05e
Deltarion Systems <deltarionsystems@gmail.com>
2024-04-22 16:53:50 +0700

    Implement RTCEncodedImage

--- src/rtcencodedimage.cpp
+++ src/rtcencodedimage.cpp
@@ -2,4 +2,147 @@
 
 RTCEncodedImage::RTCEncodedImage(QObject *parent) : QObject{parent}
 {
+    buffer_ = QByteArray();
+    encodedWidth_ = 0;
+    encodedHeight_ = 0;
+    timeStamp_ = 0;
+    captureTimeMs_ = 0;
+    ntpTimeMs_ = 0;
+    flags_ = 0;
+    encodeStartMs_ = 0;
+    encodeFinishMs_ = 0;
+    frameType_ = RTCFrameType::RTCFrameTypeEmptyFrame;
+    rotation_ = RTCVideoRotation::RTCVideoRotation_0;
+    qp_ = QVariant(0);
+    contentType_ = RTCVideoContentType::RTCVideoContentTypeUnspecified;
+}
+
+QByteArray RTCEncodedImage::buffer() const
+{
+    return buffer_;
+}
+
+void RTCEncodedImage::setBuffer(const QByteArray &buffer)
+{
+    buffer_ = buffer;
+}
+
+int32_t RTCEncodedImage::encodedWidth() const
+{
+    return encodedWidth_;
+}
+
+void RTCEncodedImage::setEncodedWidth(int32_t encodedWidth)
+{
+    encodedWidth_ = encodedWidth;
+}
+
+int32_t RTCEncodedImage::encodedHeight() const
+{
+    return encodedHeight_;
+}
+
+void RTCEncodedImage::setEncodedHeight(int32_t encodedHeight)
+{
+    encodedHeight_ = encodedHeight;
+}
+
+uint32_t RTCEncodedImage::timeStamp() const
+{
+    return timeStamp_;
+}
+
+void RTCEncodedImage::setTimeStamp(uint32_t timeStamp)
+{
+    timeStamp_ = timeStamp;
+}
+
+int64_t RTCEncodedImage::captureTimeMs() const
+{
+    return captureTimeMs_;
+}
+
+void RTCEncodedImage::setCaptureTimeMs(int64_t captureTimeMs)
+{
+    captureTimeMs_ = captureTimeMs;
+}
+
+int64_t RTCEncodedImage::ntpTimeMs() const
+{
+    return ntpTimeMs_;
+}
+
+void RTCEncodedImage::setNtpTimeMs(int64_t ntpTimeMs)
+{
+    ntpTimeMs_ = ntpTimeMs;
+}
+
+uint8_t RTCEncodedImage::flags() const
+{
+    return flags_;
+}
+
+void RTCEncodedImage::setFlags(uint8_t flags)
+{
+    flags_ = flags;
+}
+
+int64_t RTCEncodedImage::encodeStartMs() const
+{
+    return encodeStartMs_;
+}
+
+void RTCEncodedImage::setEncodeStartMs(int64_t encodeStartMs)
+{
+    encodeStartMs_ = encodeStartMs;
+}
+
+int64_t RTCEncodedImage::encodeFinishMs() const
+{
+    return encodeFinishMs_;
+}
+
+void RTCEncodedImage::setEncodeFinishMs(int64_t encodeFinishMs)
+{
+    encodeFinishMs_ = encodeFinishMs;
+}
+
+RTCFrameType RTCEncodedImage::frameType() const
+{
+    return frameType_;
+}
+
+void RTCEncodedImage::setFrameType(const RTCFrameType &frameType)
+{
+    frameType_ = frameType;
+}
+
+RTCVideoRotation RTCEncodedImage::rotation() const
+{
+    return rotation_;
+}
+
+void RTCEncodedImage::setRotation(const RTCVideoRotation &rotation)
+{
+    rotation_ = rotation;
+}
+
+QVariant RTCEncodedImage::qp() const
+{
+    return qp_;
+}
+
+void RTCEncodedImage::setQp(const QVariant &qp)
+{
+    qp_ = qp;
+}
+
+RTCVideoContentType RTCEncodedImage::contentType() const
+{
+    return contentType_;
+}
+
+void RTCEncodedImage::setContentType(const RTCVideoContentType &contentType)
+{
+    contentType_ = contentType;
 }
--- src/rtcencodedimage.h
+++ src/rtcencodedimage.h
@@ -3,7 +3,7 @@
 
 #include <QObject>
 #include <QByteArray>
-#include <QSize>
+#include <QVariant>
 
 #include "rtctypes.h"
 
@@ -21,18 +21,59 @@ class RTCEncodedImage : public QObject
      */
     explicit RTCEncodedImage(QObject *parent = nullptr);
 
-    QByteArray Buffer;
-    QSize EncodedSize;
-    quint32 TimeStamp;
-    qint64 CaptureTimeMs;
-    qint64 NtpTimeMs;
-    quint8 Flags;
-    qint64 EncodeStartMs;
-    qint64 EncodeFinishMs;
-    RTCFrameType FrameType;
-    RTCVideoRotation Rotation;
-    int Qp;
-    RTCVideoContentType ContentType;
+    QByteArray buffer() const;
+    void setBuffer(const QByteArray &buffer);
+
+    int32_t encodedWidth() const;
+    void setEncodedWidth(int32_t encodedWidth);
+
+    int32_t encodedHeight() const;
+    void setEncodedHeight(int32_t encodedHeight);
+
+    uint32_t timeStamp() const;
+    void setTimeStamp(uint32_t timeStamp);
+
+    int64_t captureTimeMs() const;
+    void setCaptureTimeMs(int64_t captureTimeMs);
+
+    int64_t ntpTimeMs() const;
+    void setNtpTimeMs(int64_t ntpTimeMs);
+
+    uint8_t flags() const;
+    void setFlags(uint8_t flags);
+
+    int64_t encodeStartMs() const;
+    void setEncodeStartMs(int64_t encodeStartMs);
+
+    int64_t encodeFinishMs() const;
+    void setEncodeFinishMs(int64_t encodeFinishMs);
+
+    RTCFrameType frameType() const;
+    void setFrameType(const RTCFrameType &frameType);
+
+    RTCVideoRotation rotation() const;
+    void setRotation(const RTCVideoRotation &rotation);
+
+    QVariant qp() const;
+    void setQp(const QVariant &qp);
+
+    RTCVideoContentType contentType() const;
+    void setContentType(const RTCVideoContentType &contentType);
+
+  private:
+    QByteArray buffer_;
+    int32_t encodedWidth_;
+    int32_t encodedHeight_;
+    uint32_t timeStamp_;
+    int64_t captureTimeMs_;
+    int64_t ntpTimeMs_;
+    uint8_t flags_;
+    int64_t encodeStartMs_;
+    int64_t encodeFinishMs_;
+    RTCFrameType frameType_;
+    RTCVideoRotation rotation_;
+    QVariant qp_;
+    RTCVideoContentType contentType_;
 };
 
 #endif // RTCENCODEDIMAGE_H
