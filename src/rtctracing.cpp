f76f81ebb3197b493feb525befde433d61953f69
Deltarion Systems <deltarionsystems@gmail.com>
2024-04-23 16:10:00 +0700

    Implement RTCTracing

--- src/rtctracing.cpp
+++ src/rtctracing.cpp
@@ -1,5 +1,27 @@
 #include "rtctracing.h"
 
+#include "rtc_base/event_tracer.h"
+
 RTCTracing::RTCTracing(QObject *parent) : QObject{parent}
 {
+}
+
+void RTCTracing::RTCSetupInternalTracer()
+{
+    rtc::tracing::SetupInternalTracer();
+}
+
+bool RTCTracing::RTCStartInternalCapture(const QString &filePath)
+{
+    return rtc::tracing::StartInternalCapture(filePath.toStdString());
+}
+
+void RTCTracing::RTCStopInternalCapture()
+{
+    rtc::tracing::StopInternalCapture();
+}
+
+void RTCTracing::RTCShutdownInternalTracer()
+{
+    rtc::tracing::ShutdownInternalTracer();
 }
